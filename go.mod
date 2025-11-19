38381735ba9915425505036f76af322901007dcc
erdedani <erdeni@usdt.kg>
2025-11-19 13:27:59 +0800

    logger, requestid, recovery

--- go.mod
+++ go.mod
@@ -3,11 +3,14 @@ module github.com/erdedan1/protohome
 go 1.24.6
 
 require (
+	github.com/google/uuid v1.6.0
 	google.golang.org/grpc v1.76.0
 	google.golang.org/protobuf v1.36.6
 )
 
 require (
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/net v0.42.0 // indirect
 	golang.org/x/sys v0.34.0 // indirect
 	golang.org/x/text v0.27.0 // indirect
---
+++ interceptors/request_id/request_id.go
@@ -0,0 +1,37 @@
+package requestid
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+const Header = "x-request-id"
+
+func UnaryServerInterceptor() grpc.UnaryServerInterceptor {
+	return func(
+		ctx context.Context,
+		req interface{},
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler,
+	) (interface{}, error) {
+
+		md, ok := metadata.FromIncomingContext(ctx)
+		if !ok {
+			md = metadata.New(nil)
+		}
+
+		ids := md.Get(Header)
+
+		if len(ids) != 0 {
+			rid := uuid.New().String()
+			md.Set(Header, rid)
+		}
+
+		ctx = metadata.NewIncomingContext(ctx, md)
+
+		return handler(ctx, req)
+	}
+}
